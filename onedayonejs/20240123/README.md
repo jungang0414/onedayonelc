002959236e37b2082b3f48035ef3ba0454ff5482
jungang0414 <dayr88520@gmail.com>
2024-01-23 14:12:43 +0800

    67. Add Binary

--- onedayonejs/20240123/README.md
+++ onedayonejs/20240123/README.md
@@ -1,3 +1,5 @@
 67. Add Binary
 
 Given two binary strings a and b, return their sum as a binary string.
+
+# 兩個二進位的字串相加 回傳相加後的二進位的數值
--- onedayonejs/20240123/index.js
+++ onedayonejs/20240123/index.js
@@ -1,3 +1,8 @@
-var addBinary = function(a, b) {
-    
+const a = 11;
+const b = 1;
+
+var addBinary = function (a, b) {
+    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2)
 };
+
+console.log(addBinary(a, b))
