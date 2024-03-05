614abdfb76cbf952a0779666a9091e8068cf3b52
jungang0414 <dayr88520@gmail.com>
2024-03-05 11:39:09 +0800

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ onedayonejs/20240305/README.md
@@ -0,0 +1,8 @@
+1750. Minimum Length of String After Deleting Similar Ends
+
+首先設定 two points 一前一後
+
+begin
+end
+
+比對前後字串是否相同，若相同則
---
+++ onedayonejs/20240305/index.js
@@ -0,0 +1,16 @@
+var minimumLength = function(s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right && s[left] === s[right]) {
+        // 
+        let char = s[left];
+        while (left <= right && s[left] === char) {
+            left++;
+        }
+        while (right >= left && s[right] === char) {
+            right--;
+        }
+    }
+
+    return right - left + 1;
+};
