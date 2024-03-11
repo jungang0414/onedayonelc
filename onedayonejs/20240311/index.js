def7b1e5f25d484744ec78f6dbf80e9d263dafa5
jungang0414 <dayr88520@gmail.com>
2024-03-11 16:59:38 +0800

    791. Custom Sort String

---
+++ onedayonejs/20240311/index.js
@@ -0,0 +1,27 @@
+let order = "cba";
+let s = "abcd";
+var customSortString = function(order, s) {
+    const charCount = {};
+    for (const char of order) {
+        charCount[char] = 0;
+    }
+
+    for (const char of s) {
+        if (charCount[char] !== undefined) {
+            charCount[char]++;
+        }
+    }
+
+    let sorted = '';
+    for (const char of order) {
+        sorted += char.repeat(charCount[char]);
+    }
+
+    for (const char of s) {
+        if (!order.includes(char)) {
+            sorted += char;
+        }
+    }
+    return sorted;
+};
+
